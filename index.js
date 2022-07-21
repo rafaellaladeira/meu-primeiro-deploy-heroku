41339f2c837d4a3d64ca4dd9c7ee80996a3bbb7d
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-07-21 11:42:05 -0300

    teste

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_req, res) => {
-    res.send('Está vivo!!');
-})
+    res.send('Está vivoooo!!');
+});
 
 app.listen(port, () => {
     console.log(`Estou na portinha ${port}`);
